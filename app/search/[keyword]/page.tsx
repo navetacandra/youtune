c4e48fe04b8bbf0a7298faeb593501e41ab2950a
Revo Sasta Azzaro <revosastaazzaro16@gmail.com>
2024-06-08 19:24:48 +0700

    feat: create a search page

---
+++ app/search/[keyword]/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Fragment } from "react";
+import { Content, ContentDetail } from "@/app/api/featured/route";
+import useSWR from "swr";
+import Image from "next/image";
+
+const fetcher = (url:string) => fetch(url).then(r => r.json())
+
+export default function Page({ params: { keyword } }: { params: { keyword: string } }) {
+    
+
+  const { data, error, isLoading } = useSWR(`/api/search/${decodeURI(keyword)}`, fetcher)
+  
+  if (error) return <p>Erorrrr</p>
+  if (isLoading) return <p>loadingggg</p>
+  
+  return (
+    <>
+      {
+        data.results.map((d:Content, i:number) => (
+          <Fragment key={i}>
+          <h1>{d.category}</h1>
+          {d.contents.map((c: ContentDetail, j: number) => (
+            <Fragment key={j}>
+              <Image src={c.thumbnail} alt="..." width={300} height={250} className="mt-20"/>
+              <h2>{`${c.title.text}`}</h2>
+            </Fragment>
+          ))}
+          </Fragment>
+        ))
+  }
+    </>
+  );
+}
---
+++ components/Search.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { usePathname, useRouter } from "next/navigation"
+import { FormEvent, useRef } from "react"
+
+const Search = () => {
+
+    const query = useRef<HTMLInputElement|null>(null)
+    const router = useRouter()
+
+    const pathname = usePathname()
+    const q = pathname.match(/\/search\/(.*)\/?/);
+    const p = q ? q[1] : ''
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        router.push(`/search/${encodeURI(query.current?.value.trim()|| " ")}`)
+    }
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input type='text' ref={query} defaultValue={p} className="text-black"></input>
+    </form>
+  )
+}
+
+export default Search
