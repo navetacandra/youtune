43317a8075a99e4125874e4118599f0dd5fb3462
navetacandra <naveta.cand@gmail.com>
2024-07-10 10:41:14 +0700

    fix: change ytdl-core to YouTube API

--- app/api/player/[id]/route.ts
+++ app/api/player/[id]/route.ts
@@ -1,6 +1,59 @@
 import fapi from "@/utils/api/fapi";
+import { StreamingFormat } from "@/utils/functions";
 import { NextResponse } from "next/server";
-import { chooseFormat, getInfo as ytdlGetInfo } from "ytdl-core";
+
+const getFormats = async (videoId: string): Promise<StreamingFormat> => {
+  const apiKey = "AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc";
+  const headers = {
+    "X-YouTube-Client-Name": "5",
+    "X-YouTube-Client-Version": "19.09.3",
+    Origin: "https://www.youtube.com",
+    "User-Agent":
+      "com.google.ios.youtube/19.09.3 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)",
+    "content-type": "application/json",
+  };
+
+  const b = {
+    context: {
+      client: {
+        clientName: "IOS",
+        clientVersion: "19.09.3",
+        deviceModel: "iPhone14,3",
+        userAgent:
+          "com.google.ios.youtube/19.09.3 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)",
+        hl: "en",
+        timeZone: "UTC",
+        utcOffsetMinutes: 0,
+      },
+    },
+    videoId,
+    playbackContext: {
+      contentPlaybackContext: { html5Preference: "HTML5_PREF_WANTS" },
+    },
+    contentCheckOk: true,
+    racyCheckOk: true,
+  };
+
+  const res = await fetch(
+    `https://www.youtube.com/youtubei/v1/player?key${apiKey}&prettyPrint=false`,
+    { method: "POST", body: JSON.stringify(b), headers },
+  );
+  if (!res.ok) throw new Error(res.statusText);
+  const json = await res.json();
+  return json.streamingData.adaptiveFormats as StreamingFormat;
+};
+
+const chooseFormat = (formats: StreamingFormat[]) => {
+  const level: { [key: string]: number } = {
+    AUDIO_QUALITY_HIGH: 1,
+    AUDIO_QUALITY_MEDIUM: 2,
+    AUDIO_QUALITY_LOW: 3,
+    "": 4,
+  };
+  return formats.sort(
+    (a, b) => level[a.audioQuality || ""] - level[b.audioQuality || ""],
+  )[0];
+};
 
 export const GET = async (
   req: Request,
@@ -50,8 +103,9 @@ export const GET = async (
       chunks,
       ok = false;
     while (!ok) {
-      format = chooseFormat((await ytdlGetInfo(id)).formats, {
-        quality: "highestaudio",
+      const tform = await getFormats(id);
+      format = chooseFormat(tform, {
+        filter: "audio",
       });
       meta = {
         duration: Number(format?.approxDurationMs) / 1000,
--- utils/functions.ts
+++ utils/functions.ts
@@ -4,6 +4,30 @@ type Chunk = {
   end: number;
 };
 
+export type StreamingFormat = {
+  itag: number;
+  url: string;
+  mimeType: string;
+  bitrate: number;
+  width?: number;
+  height?: number;
+  initRange: { start: string; end: string };
+  indexRange: { start: string; end: string };
+  lastModified: string;
+  contentLength: string;
+  quality: string;
+  fps?: number;
+  qualityLabel: string;
+  projectionType: string;
+  averageBitrate: number;
+  approxDurationMs: string;
+  highReplication?: boolean;
+  audioQuality?: string;
+  audioSampleRate?: string;
+  audioChannels?: number;
+  loudnessDb?: number;
+};
+
 export type Track = {
   videoId: string;
   title: { text: string };
